ba641c1dca50e7ce82f2a643d953fab427592d4e
Sergiobudi <sergio92_47@hotmail.com>
2024-09-23 11:21:11 +0200

    bien

--- BLOQUE-1/Basic.js
+++ BLOQUE-1/Basic.js
@@ -1,14 +1,34 @@
 // EJERCICIO 1//
 
-/*1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
-1.2 Crea una variable llamada x, asigna el valor 50 a ella.
-1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
-1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.*/
+let myFavoriteHero = "Hulk"
+let x = 50;
+let h = 5;
+let y = 10;
+let z = h + y;
 
-let x=50
-let h=5
-let y=10
-let z= "h"+"y"
 
-console.log(z)
+//EJERCICIO 2//
+
+/*1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
+const character = {name: 'Jack Sparrow', age: 10};*/
+
+const character = {name: "Jack Sparrow", age: 25};
+
+/*1.2 Declara 3 variables con los nombres y valores siguientes 
+firstName = 'Jon'; 
+lastName = 'Snow'; 
+age = 24; 
+Muestralos por consola de esta forma: 
+'Soy Jon Snow, tengo 24 años y me gustan los lobos.'*/
+
+let firstName = "Jon";
+let lastName = "Snow";
+age = 24;
+
+
+
+
+
+
+
 
