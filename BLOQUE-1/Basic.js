dc730cbdaa579ca54629770d24d79ee8ba7527ba
Sergiobudi <sergio92_47@hotmail.com>
2024-09-23 20:14:41 +0200

    seguire

--- BLOQUE-1/Basic.js
+++ BLOQUE-1/Basic.js
@@ -50,6 +50,14 @@ let globalBasePrice= 25000
 const car1 = {name: "BMW m&m", basePrice: 50000, finalPrice: 60000};
 const car2 = {name: "Chevrolet Corbina", basePrice: 70000, finalPrice: 80000};
 
+const totalCar1 = (car1.basePrice + globalBasePrice)=(finalPrice)
+
+console.log(totalCar1)
+
+
+
+
+
 
 
 
