606b71bf7023ec5b2e6f93766dd480012000cf48
Sergiobudi <sergio92_47@hotmail.com>
2024-09-23 11:46:29 +0200

    consola

--- BLOQUE-1/Basic.js
+++ BLOQUE-1/Basic.js
@@ -29,6 +29,10 @@ let textoEntero = `Soy ${firstName} ${lastName}, tengo ${age} años y me gustan 
 
 console.log(textoEntero);
 
+/*1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
+ambos juguetes.
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29}*/
 
 
 
