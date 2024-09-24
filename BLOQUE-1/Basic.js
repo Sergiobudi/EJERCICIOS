daa60f62d3cf75d1a446460d0c04235ea912d738
Sergiobudi <sergio92_47@hotmail.com>
2024-09-24 19:05:34 +0200

    siguientes

--- BLOQUE-1/Basic.js
+++ BLOQUE-1/Basic.js
@@ -6,6 +6,7 @@ let h = 5;
 let y = 10;
 let z = h + y;
 
+//*console.log(h+y)*//
 
 //EJERCICIO 2//
 
@@ -27,7 +28,7 @@ age = 24;
 
 let textoEntero = `Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos.`;
 
-console.log(textoEntero);
+//*console.log(textoEntero)*//
 
 /*1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
 ambos juguetes.
@@ -37,7 +38,7 @@ const toy2 = {name: 'Rallo mcKing', price: 29}*/
 const toy1 = {name: "Buss myYear", price:19};
 const toy2 = {name: "Rallo mcKing", price:29}
 
-console.log(toy1.price+toy2.price)
+//*console.log(toy1.price+toy2.price)*//
 
 /* 1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
 y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
@@ -50,9 +51,56 @@ let globalBasePrice= 25000
 const car1 = {name: "BMW m&m", basePrice: 50000, finalPrice: 60000};
 const car2 = {name: "Chevrolet Corbina", basePrice: 70000, finalPrice: 80000};
 
-const totalCar1 = (car1.basePrice + globalBasePrice)=(finalPrice)
 
-console.log(totalCar1)
+
+
+
+
+// EJERCICIO 3 //
+
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante console//
+
+console.log(10*5)
+
+//1.2 Divide 10 por 2 y muestra el resultado en un console.//
+
+console.log(10/2)
+
+//1.3 Muestra mediante un console el resto de dividir 15 por 9.//
+
+console.log(15 % 3)
+
+/*1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+teniendo dos variables p = 10 y j = 5.*/
+
+
+
+/*1.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5.*/
+
+
+// EJERCICIO 4 //
+
+//1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola//
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+//*console.log(avengers[0])*//
+
+//1.2 Cambia el primer elemento de avengers a "IRONMAN"//
+
+let cambio1 = avengers.shift()
+
+let nuevoNombre = (avengers.unshift("IRONMAN"));
+
+//*console.log(cambio1)
+
+//*console.log(avengers)
+
+
+
+
+
 
 
 
