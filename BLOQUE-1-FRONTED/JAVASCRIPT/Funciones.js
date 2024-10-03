b0495ff62da3719133c12a0e13a6bf47b140915f
Sergiobudi <sergio92_47@hotmail.com>
2024-10-03 11:57:11 +0200

    otra

--- BLOQUE-1-FRONTED/JAVASCRIPT/Funciones.js
+++ BLOQUE-1-FRONTED/JAVASCRIPT/Funciones.js
@@ -5,11 +5,6 @@
 /*Iteración #1: Buscar el máximo
 Completa la función que tomando dos números como argumento devuelva el más alto*/
 
-function sum(numberOne , numberTwo) {
-   return Math.max (numberOne, numberTwo)
-
-}
-
-console.log,sum(10,20) 
+console.log(dddddsds)
 
 
